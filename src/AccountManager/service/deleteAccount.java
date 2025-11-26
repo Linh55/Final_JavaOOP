297b20329c363eea57c2a52c986e1ecb853219a6
Linh55 <khlinh41@gmail.com>
2025-11-26 21:20:16 +0700

    Update deleteAccount.java

--- src/AccountManager/service/deleteAccount.java
+++ src/AccountManager/service/deleteAccount.java
@@ -1,4 +1,24 @@
 package AccountManager.service;
 
+import AccountManager.Account;
+import AccountManager.data.AccountDatabase;
+
+import java.util.Scanner;
+
 public class deleteAccount {
+    private AccountDatabase accountDB = AccountDatabase.getAccountDB();
+    String username;
+    Scanner sc = new Scanner(System.in);
+
+    public void delete(){
+        System.out.print("Nhập tên đăng nhập tài khoản muốn xóa: ");
+        username = sc.nextLine();
+        Account a = accountDB.findAccountByUsername(username);
+        if(a == null){
+            System.out.println("Không tìm thấy tài khoản " + username);
+            return;
+        }
+        accountDB.deleteAccount(username);
+        System.out.println("Đã xóa tài khoản thành công!");
+    }
 }
